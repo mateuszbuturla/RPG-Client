5c740006237b7512f13495ea158855cb6c58b2f0
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-08-26 18:40:29 +0200

    fix/remove wrong '}' char

--- assets/Script/Player/PlayerMovement.ts
+++ assets/Script/Player/PlayerMovement.ts
@@ -53,7 +53,6 @@ export default class NewClass extends cc.Component {
         global.socket.emit("keyPress", { inputId: "left", state: true });
       if (this.keys.has(cc.macro.KEY.w))
         global.socket.emit("keyPress", { inputId: "up", state: true });
-      }
     }
   }
 
